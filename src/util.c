1fd2bad7c17cc41323d454b2510e53e70a846ccf
Jeff Larson <thejefflarson@gmail.com>
2011-05-12 20:16:11 -0400

    utils

---
+++ src/util.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "util.h"
+
+char*
+copy_string(char *src){
+  int len = strlen(src);
+  char *dest;
+  if(!(dest = malloc(len + 1)))
+    return NULL;
+  memcpy(dest, src, len);
+  dest[len] = '\0';
+  return dest;
+}
---
+++ src/util.h
@@ -0,0 +1,15 @@
+#ifndef SIMPLE_TILES_UTIL_H
+#define SIMPLE_TILES_UTIL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+char*
+copy_string(char *src);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
