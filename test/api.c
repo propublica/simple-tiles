03744ed41ec679cfd23726ed2add5d4a46ef0a63
Jeff Larson <thejefflarson@gmail.com>
2011-06-08 22:59:00 -0400

    track render time

--- test/api.c
+++ test/api.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <unistd.h>
+#include <time.h>
 #include <simple-tiles/simple_tiles.h>
 
 int
@@ -19,8 +20,9 @@ main(){
   simplet_map_add_style(map, "weight", "0.3");
   simplet_map_add_style(map, "fill",   "#061F37ff");
   simplet_map_add_style(map, "stroke", "#ffffff");
-
+  clock_t start = clock();
   if(simplet_map_is_valid(map))
     simplet_map_render_to_png(map, "./out.png");
+  printf("render time: %f", (float) (clock() - start) / CLOCKS_PER_SEC);
   simplet_map_free(map);
 }
