deb84ca5a756c58f0b6c71b0619d17e7e97d3df9
Jeff Larson <thejefflarson@gmail.com>
2011-09-14 13:03:36 -0400

    fix to tests

--- test/api.c
+++ test/api.c
@@ -12,11 +12,10 @@ main(){
 
   simplet_map_set_size(map, 256, 256);
   simplet_map_set_slippy(map, 0, 0, 0);
-  simplet_map_add_layer(map, "../data/10m_admin_0_countries.shp");
-  simplet_map_add_filter(map,  "SELECT * from '10m_admin_0_countries'");
+  simplet_map_add_layer(map, "../data/ne_10m_admin_0_countries.shp");
+  simplet_map_add_filter(map,  "SELECT * from 'ne_10m_admin_0_countries'");
   simplet_map_add_style(map, "weight", "0.1");
   simplet_map_add_style(map, "fill",   "#061F3799");
-  simplet_map_add_style(map, "seamless", "true");
   if(simplet_map_is_valid(map))
     simplet_map_render_to_png(map, "./out.png");
   simplet_map_free(map);
