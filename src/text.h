e7d83fd8c399a52f39e7f8b407f02ae6977804f4
Jeff Larson <thejefflarson@gmail.com>
2011-11-11 18:59:33 -0500

    text.c

---
+++ src/text.h
@@ -0,0 +1,17 @@
+#ifndef _SIMPLE_TEXT_H
+#define _SIMPLE_TEXT_H
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+void
+simplet_try_placement(simplet_placement_t, simplet_list_t *placements);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
