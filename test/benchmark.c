a6cd4afe40c153ad006ef885a69075b462681c3b
Jeff Larson <thejefflarson@gmail.com>
2011-11-13 01:18:05 -0500

    path

--- test/benchmark.c
+++ test/benchmark.c
@@ -35,7 +35,7 @@ initialize_map(simplet_map_t *map){
   simplet_map_set_size(map, 256, 256);
   simplet_map_set_slippy(map, 0, 1, 2);
   simplet_map_add_layer(map, "../data/ne_10m_admin_0_countries.shp");
-  simplet_map_add_filter(map, "SELECT * from 'ne_10m_admin_0_countries'");
+  simplet_map_add_filter(map,  "SELECT * from 'ne_10m_admin_0_countries'");
   simplet_map_add_style(map, "weight", "0.1");
   simplet_map_add_style(map, "fill",   "#061F37ff");
 }
