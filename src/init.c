b40548deeddf16e11ff9fc3c5677deafc3b53ba2
Jeff Larson <thejefflarson@gmail.com>
2012-02-03 15:50:18 -0500

    init.c docs

--- src/init.c
+++ src/init.c
@@ -4,15 +4,18 @@
 
 static int initialized = 0;
 
+// The atexit handler used to close all connections to open data stores
 static void
 cleanup(){
 	for(int i = 0; i < OGRGetOpenDSCount(); i++)
-    while(OGRGetOpenDS(i) && OGR_DS_GetRefCount(OGRGetOpenDS(i))) //ughh
+    while(OGRGetOpenDS(i) && OGR_DS_GetRefCount(OGRGetOpenDS(i)))
       OGRReleaseDataSource(OGRGetOpenDS(i));
   assert(!OGRGetOpenDSCount());
   OGRCleanupAll();
 }
 
+
+// Initialize libraries, register the atexit handler and set up error reporting.
 void
 simplet_init(){
   if(initialized) return;
