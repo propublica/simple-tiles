1d71aa0a39ac785326cbaa6e4d60e3e2c937a58f
Jeff Larson <thejefflarson@gmail.com>
2012-02-22 13:48:01 -0500

    testing docs

--- test/test.h
+++ test/test.h
@@ -6,14 +6,18 @@
 #include <assert.h>
 #include <simple-tiles/simple_tiles.h>
 
+// simple testing macro that prints the name of the test
+// and a cute checkmark if it passed
 #define test(fn) \
         printf("\x1b[33m" # fn "\x1b[0m "); \
         test_##fn(); \
         puts("\x1b[1;32m✓ \x1b[0m");
 
+// Proto definition.
 #define TASK(name) \
   void run_task_##name()
 
+// List of tasks to run.
 TASK(list);
 TASK(layer);
 TASK(filter);
