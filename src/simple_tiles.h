726edace3a6c017020c01c6213bbca1383bf4848
Jeff Larson <thejefflarson@gmail.com>
2013-03-18 16:30:40 -0400

    include config.h

--- src/simple_tiles.h
+++ src/simple_tiles.h
@@ -1,15 +1,4 @@
 #ifndef _SIMPLE_TILES_H
 #define _SIMPLE_TILES_H
 #include "map.h"
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#define SIMPLE_TILES_VERSION "0.1.0"
-
-#ifdef __cplusplus
-}
-#endif
-
 #endif
